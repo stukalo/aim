2196b0767f2d50096414dd9a1d35a38086c43c98
stukalo <stukalo.oleksandr@gmail.com>
2022-01-20 00:51:08 +0200

    update common

--- common/src/index.ts
+++ common/src/index.ts
@@ -5,11 +5,13 @@ export * from './errors/database-connection-error';
 export * from './errors/not-authorized-error';
 export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
+export * from './errors/forbidden-error';
 
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
+export * from './middlewares/protected-resource';
 
 export * from './events/base-listener';
 export * from './events/base-publisher';
